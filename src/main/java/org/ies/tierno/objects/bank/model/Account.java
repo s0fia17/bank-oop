0d75f271273c575f1968e017f7417e34bcfc463a
Mikel <mikel.san@educa.madrid.org>
2025-11-20 11:10:32 +0100

    deposit y withdraw

--- src/main/java/org/ies/tierno/objects/bank/model/Account.java
+++ src/main/java/org/ies/tierno/objects/bank/model/Account.java
@@ -14,8 +14,22 @@ public class Account {
     }
 
     public void showInfo() {
-        System.out.print("IBAN: " + iban + ". Saldo: " + balance +". Titular: ");
+        System.out.print("IBAN: " + iban + ". Saldo: " + balance + ". Titular: ");
         customer.showInfo();
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        } else {
+            System.out.println("No hay suficiente saldo");
+            return false;
+        }
     }
 
     public String getIban() {
